5e30ef2234956109d3210f26db1231afda7e6bd9
po100lit <po100lit@mail.ru>
2022-07-09 17:17:32 +0600

    обновил задачу о N разрядности в K системе

--- my_first_programms/n-razradov-k-osnovanie.py
+++ my_first_programms/n-razradov-k-osnovanie.py
@@ -5,11 +5,12 @@
 import string
 import textwrap
 
+n = int(input('Введите Количество знаков '))
+k = int(input('Введите разрядность системы '))
+
 
 def main():
     digits = string.digits + string.ascii_uppercase
-    n = int(input('Введите количество разрядов: '))
-    k = int(input('Введите основание системы счисления: '))
     low_limit = int(str(10 ** (n - 1)), k)
     high_limit = int(str(10 ** n), k)
     numbers_list = []
@@ -21,9 +22,22 @@ def main():
         if '00' not in res:
             numbers_list.append(res)
     print(f'Количество {n}-значных чисел без двух нулей подряд: {len(numbers_list)}')
+
     is_numbers_print = input('Вывести числа в консоль? y/n: ')
     if is_numbers_print.lower() == 'y':
         print(textwrap.fill(' '.join(numbers_list), 120))
+
+    # Решение Олега (Htyg) через рекурсию
+    def True_numbers(n, k):  # Количество "подходящих" чисел
+        if n == 1:
+            return k - 1  # Количество не нулевых однозначных чисел
+        if n == 2:
+            return (k - 1) * k  # Количество двухзначных чисел
+        return ((k - 1) *  # Количество чисел которые могут стоять первыми(в самом большом разряде)
+                (True_numbers(n - 1, k) +  # Количество чисел если последняя цифа 0
+                 True_numbers(n - 2, k)))  # Количество чисел если последняя цифра не 0
+
+    print(True_numbers(n, k))
 
 
 if __name__ == "__main__":
