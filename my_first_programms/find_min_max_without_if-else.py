45d57b531d078dbd70427d097649b4508b6ba2ea
po100lit <po100lit@mail.ru>
2022-07-10 21:14:33 +0600

    add more solutions to find_min_max_without_if-else.py

--- my_first_programms/find_min_max_without_if-else.py
+++ my_first_programms/find_min_max_without_if-else.py
@@ -4,6 +4,10 @@ def main():
     big = (a + b) / 2 + abs(a - b) / 2
     low = (a + b) / 2 - abs(a - b) / 2
     print(big, low)
+    print('max = ', (a, b)[a < b])
+    print('min = ', (a, b)[a > b])
+    print('max = ', (a < b) * b + (a > b) * a)
+    print('min = ', (a < b) * a + (a > b) * b)
 
 
 if __name__ == "__main__":
