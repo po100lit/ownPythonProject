45d2a692f9645264feaa62b80f677a883a6138b8
po100lit <po100lit@mail.ru>
2022-07-15 16:21:09 +0600

    update parsing slovar

--- parsing_slovar/main.py
+++ parsing_slovar/main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import os.path
-import time
 
 host = 'https://wordsonline.ru'
 headers = {
@@ -13,11 +12,11 @@ headers = {
 words_set = set()
 
 
-def get_index_page():
+def get_index_page() -> str:
     url = 'https://wordsonline.ru/dicts/orthography.html'
     r = requests.get(url=url, headers=headers)
     if r.status_code == 200:
-        if os.path.exists('HTML/index.html') and os.path.getsize('HTML/index.html') > 100:
+        if os.path.exists('HTML/index.html'):
             with open('HTML/index.html', 'r', encoding='utf-8') as file:
                 index_html = file.read()
         else:
@@ -30,7 +29,7 @@ def get_index_page():
     return index_html
 
 
-def get_alphabet_pages():
+def get_alphabet_pages() -> list[str]:
     alphabet_links = []
     html_text = get_index_page()
     soup = BeautifulSoup(html_text, 'lxml')
@@ -40,23 +39,36 @@ def get_alphabet_pages():
     return alphabet_links
 
 
-def get_pagination(url):
-    pass
+def get_pagination():
+    pagination_pages = []
+    for i in get_alphabet_pages():
+        r = requests.get(url=i, headers=headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        try:
+            last_page = int(soup.find('ul', class_='pagination').find_all('a')[-1].text)
+            for j in range(2, last_page + 1):
+                pagination_pages.append(f'{i}?page={j}')
+        except AttributeError:
+            print('Just one page found')
+    return pagination_pages + get_alphabet_pages()
 
 
 def get_words():
-    links = get_alphabet_pages()
+    links = get_pagination()
+    count = 1
     for i in links:
-        # TODO: пройтись по пагинации
         r = requests.get(url=i, headers=headers)
         soup = BeautifulSoup(r.text, 'lxml')
         words = soup.find('ul', class_='list-unstyled').findAll('li')
         for word in words:
             words_set.add(word.text)
-        time.sleep(0.5)
-        print(f'Scrapping page {i[-1]}')
-    print(words_set)
+        # time.sleep(0.1)
+        print(f'Scrapping page {count}/{len(links)}')
+        count += 1
     print(len(words_set))
+    with open('DATA/words.txt', 'w', encoding='utf-8') as file:
+        data = file.write(','.join(words_set))
+        return data
 
 
 def main():
---
+++ parsing_slovar/stat_count.py
@@ -0,0 +1,31 @@
+def get_words():
+    with open('DATA/words.txt', encoding='utf-8') as file:
+        data = file.read()
+    return data
+
+
+def letters_stat():
+    letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    stat_dict = {}
+    stat_string = get_words()
+    for letter in letters:
+        stat_dict[letter] = stat_string.count(f'{letter}')
+    # sort_by_quantity_high_low = sorted(stat_dict.items(), key=lambda x: x[1], reverse=True)
+    # sort_by_quantity_low_high = sorted(stat_dict.items(), key=lambda x: x[1])
+    # for i in range(10):
+    #     print(sort_by_quantity_high_low[i])
+    # print()
+    # for i in range(10):
+    #     print(sort_by_quantity_low_high[i])
+    max_word_length = 0
+    for i in get_words().split(','):
+        max_word_length = max(max_word_length, len(i))
+    print(max_word_length)
+
+
+def main():
+    letters_stat()
+
+
+if __name__ == "__main__":
+    main()
